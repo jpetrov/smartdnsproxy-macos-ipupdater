1370db085eaf9a156b65706630a8580f545c6afa
Jovan Petrov <jpetrov@gmail.com>
2023-11-12 02:19:27 +0100

    Create update_dnsapi.sh

---
+++ update_dnsapi.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+WIFI_SSID_RAW=$(/System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -I | awk -F: '/ SSID/{print $2}')
+WIFI_SSID=${WIFI_SSID_RAW:1}
+echo "Current Wi-Fi SSID: [$WIFI_SSID]"
+
+TARGET_SSID="foo" #change this value to home WiFi name
+echo "Target SSID: [$TARGET_SSID]"
+
+if [[ $TARGET_SSID == $WIFI_SSID ]] 
+
+then
+    echo "We are Home, changing IP"
+
+    // run commands
+    curl https://www.smartdnsproxy.com/api/IP/update/CHANGETHIS
+
+else
+    echo "We are not Home, not changing IP"
+
+fi
